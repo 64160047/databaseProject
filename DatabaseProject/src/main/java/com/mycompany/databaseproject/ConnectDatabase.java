84fcbf9914d6001bbd66883e199aefb97e6237aa
Titapon Keawbua <64160047@go.buu.ac.th>
2023-09-18 22:04:29 +0700

    Insertdatabase

--- DatabaseProject/src/main/java/com/mycompany/databaseproject/ConnectDatabase.java
+++ DatabaseProject/src/main/java/com/mycompany/databaseproject/ConnectDatabase.java
@@ -1,21 +1,19 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.databaseproject;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author minnie
  */
 public class ConnectDatabase {
-
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         Connection conn = null;
         String url = "jdbc:sqlite.dcoffee.db";  
         try {
@@ -37,4 +35,5 @@ public class ConnectDatabase {
             }
         }
     }
+    
 }
